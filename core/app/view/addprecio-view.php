174365907309900797022e661a70e061f51485e5
Edixon <payruz16@gmail.com>
2021-04-13 14:25:55 -0500

    agregado: precio, tipo,

---
+++ core/app/view/addprecio-view.php
@@ -0,0 +1,40 @@
+
+               <div class="card-body collapse show" id="collapse1">
+                        <form action="index.php?action=addPrecio" method="post">
+                              <div class="row">
+                                 
+
+                                 <div class="col-md-4 mb-3">
+                                       <label for="validationCustom01">Tipo</label>
+                                       <input type="text" class="form-control" id="id_tipo" placeholder="id_tipo" name="id_tipo" required>
+                                       <div class="valid-feedback">
+                                          Looks good!
+                                       </div>
+                                    </div>
+                                 <!-- col -->
+                                 <div class="col-md-4 mb-3">
+                                       <label for="validationCustom01">Precio</label>
+                                       <input type="text" class="form-control" id="precio" placeholder="precio" name="precio" required>
+                                       <div class="valid-feedback">
+                                          Looks good!
+                                       </div>
+                                    </div>
+                                 <!-- col --
+                                 <div class="col-md-4 mb-3">
+                                       <label for="validationCustom01">CC</label>
+                                       <input type="text" class="form-control" id="cc" placeholder="cc" name="cc" required>
+                                       <div class="valid-feedback">
+                                          Looks good!
+                                       </div>
+                                    </div>
+                                 -- col -->
+                              </div>
+                            
+
+                               
+
+                               <button type="submit" class="btn btn-success active btn-block mg-b-10">Enviar</button>
+
+
+                        </form>
+                </div>
