94ec4e8e8050d21fb086d6f3a9807d812eac53c8
Edixon <payruz16@gmail.com>
2021-05-05 16:24:25 -0500

    api

--- core/app/action/Cita/AddCitaLavado-action.php
+++ core/app/action/Cita/AddCitaLavado-action.php
@@ -21,20 +21,42 @@ $aux=$cita->add();
 
 
 if($aux[0]==1){
-   core::alert("Su cita ha ingresado con exito al sistema");
+   core::alert("Su lavado ha ingresado con exito al sistema");
    print "<script>window.location='index.php?view=Cita/UserCita';</script>";
+
+   $cliente=ClienteData::getByCC($cedula);
+
+   $data = [
+      'phone' => '573015256417', // Receivers phone
+  
+      'body' => "EL CLIENTE *".$cliente->nombre."* SOLICITA UN LAVADO PARA LAS *".$cita->fechapedida."*", // Message
+  ];
+  $json = json_encode($data); // Encode data to JSON
+  // URL for request POST /message
+  $token = ' ihdrcqqh6f0routa';
+  $instanceId = '265655';
+  $url = 'https://api.chat-api.com/instance'.$instanceId.'/message?token='.$token;
+  // Make a POST request
+  $options = stream_context_create(['http' => [
+          'method'  => 'POST',
+          'header'  => 'Content-type: application/json',
+          'content' => $json
+      ]
+  ]);
+  // Send a request
+  $result = file_get_contents($url, false, $options);
    
 }else{
    core::alert("Error al ingresar su Cita");
-print "<script>window.location='index.php?view=Cita/UserCita';</script>";
+   print "<script>window.location='index.php?view=Cita/UserCita';</script>";
 }
 
 
 }else {
 
-   core::alert("Esta siendo redireccionado al registo para poder solicitar Lavado");
+   core::alert("Usuario no registrado");
 
-   print "<script>window.location='index.php?view=User/UserAddCliente&cedula=".$cedula."';</script>";
+   print "<script>window.location='index.php?view=Cita/UserAddCliente&cedula=".$cedula."';</script>";
 
 }
 ?>
--- core/app/action/Cita/AddClienteCita-action.php
+++ core/app/action/Cita/AddClienteCita-action.php
@@ -16,4 +16,34 @@ $cliente->celular=$celular;
 
 $aux=$cliente->add();
 
-print "<script>window.location='index.php?view=Cita/UserCita';</script>";
+
+
+if($aux[0]==1){
+   core::alert("Por Favor Ingrse Nuevamente solicitud de lavado");
+   print "<script>window.location='index.php?view=Cita/UserCita';</script>";
+
+   $data = [
+    'phone' => '573015256417', // Receivers phone
+ 
+    'body' => "EL CLIENTE *".$nombre."* *".$apellido."* SE ACABA DE REGISTAR", // Message
+ ];
+ $json = json_encode($data); // Encode data to JSON
+ // URL for request POST /message
+ $token = ' ihdrcqqh6f0routa';
+ $instanceId = '265655';
+$url = 'https://api.chat-api.com/instance'.$instanceId.'/message?token='.$token;
+ // Make a POST request
+ $options = stream_context_create(['http' => [
+        'method'  => 'POST',
+        'header'  => 'Content-type: application/json',
+        'content' => $json
+    ]
+ ]);
+ 
+  // Send a request
+  $result = file_get_contents($url, false, $options);
+   
+}else{
+   core::alert("Error al Registrar");
+   print "<script>window.location='index.php?view=Cita/UserCita';</script>";
+}
